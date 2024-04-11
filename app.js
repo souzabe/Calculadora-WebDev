cca45e21c6a2964c550f1a3d1fac7c7f57d9bd16
souzabe <jaja013souza@gmail.com>
2024-04-11 01:57:10 -0300

    diahdisa

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function clicar(){
+function soma() {
     // Entrada
     var n1 = parseFloat(document.getElementById('numero1').value)
     var n2 = parseFloat(document.getElementById('numero2').value)
@@ -9,4 +9,42 @@ function clicar(){
     // saida
    resultado.innerText =`O Resultado é ${soma}` 
 
+   
+
+}
+function sub(){
+
+    var n1 = parseFloat(document.getElementById('numero1').value)
+        var n2 = parseFloat(document.getElementById('numero2').value)
+        var resultado = document.getElementById('resultado')
+    
+        var sub = n1 - n2
+    
+       
+       resultado.innerText =`O Resultado é ${sub}` 
+}
+
+function mult(){
+
+    var n1 = parseFloat(document.getElementById('numero1').value)
+     var n2 = parseFloat(document.getElementById('numero2').value)
+     var resultado = document.getElementById('resultado')
+    
+     var mult = n1 * n2
+    
+     // saida
+    resultado.innerText =`O Resultado é ${mult}` 
+}
+
+
+function div(){
+
+    var n1 = parseFloat(document.getElementById('numero1').value)
+     var n2 = parseFloat(document.getElementById('numero2').value)
+     var resultado = document.getElementById('resultado')
+ 
+     var div = n1 / n2
+ 
+     // saida
+    resultado.innerText =`O Resultado é ${div}` 
 }
