fb74f5bbac70efa03025c6ab8fe7258b3aeff32d
souzabe <jaja013souza@gmail.com>
2024-04-10 22:19:29 -0300

    cal

---
+++ app.js
@@ -0,0 +1,10 @@
+function clicar(){
+    // Entrada
+    var numero1 = document.getElementById('numero1').value
+    var numero2 = document.getElementById('numero2').value
+
+    // saida
+    console.log(numero1)
+    console.log(numero2)
+
+}
